6f7b027b07f49155747f104ea4193e58a2296776
szepzsofia <szepzsofia351@gmail.com>
2022-09-18 14:25:31 +0200

    javitott cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS leírása helytelen, helyes: N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' //hiányzik a végéről a ;
-    for (int i = 0;) //hiányoznak a for ciklus értékei
+    int *b = new int[N_ELEMENTS]; //NELEMENTS leírása helytelen, helyes: N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'; //hiányzik a végéről a ;
+    for (int i = 0; i; i++) //hiányoznak a for ciklus értékei
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" //nincs valódi kiiratás és nincs sor végi ;
+        std::cout << "Ertek:" << b[i] << std::endl; //nincs valódi kiiratás és nincs sor végi ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //N_ELEMENTS után ; kell
+    for (int i = 0; i < N_ELEMENTS; i++) //N_ELEMENTS után ; kell
     {
-        atlag += b[i] //hiányzik a sor végi ;
+        atlag += b[i]; //hiányzik a sor végi ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
